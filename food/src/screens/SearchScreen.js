3a02abe72caa38afdd86ef954e892191782d3d24
Giorgio-Zarantonello <giorgio.zarantonello@gmail.com>
2023-04-14 23:20:00 +0200

    last commit of the day

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,20 +1,30 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
-import SearchBar from "../components/SearchBar";
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
-    const [Term, setTerm] = useState('');
+  const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
 
-    return (
-        <View>
-            <SearchBar searchTerm={Term} onSearchTermChange={newSearchTerm => setTerm(newSearchTerm)}
-                onTermSubmit={() => console.log('term submitted')}
+  const searchApi = async () => {
+    const response = await yelp.get('/search', {
+      params: {
+        limit: 50,
+        term,
+        location: 'san jose'
+      }
+    });
+    setResults(response.data.businesses);
+  };
 
-            />
-            <Text>Search Screen</Text>
-            <Text>{Term}</Text>
-        </View>
-    )
+  return (
+    <View>
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      <Text>Search Screen</Text>
+      <Text>We have found {results.length} results</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
