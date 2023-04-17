0393b0859a8c4d4e5c0dd86075ab3358de23de83
Giorgio-Zarantonello <giorgio.zarantonello@gmail.com>
2023-04-17 16:34:36 +0200

    Added Dummy Error Handling

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -6,23 +6,39 @@ import yelp from '../api/yelp';
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const searchApi = async () => {
-    const response = await yelp.get('/search', {
-      params: {
-        limit: 50,
-        term,
-        location: 'san jose'
-      }
-    });
-    setResults(response.data.businesses);
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          //term : term in ES6 Syntax
+          term,
+          location: 'san jose'
+        }
+      });
+      setResults(response.data.businesses);
+    }
+    catch (e) {
+      setErrorMessage('Something went Wrong');
+    }
   };
 
   return (
     <View>
-      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
-      <Text>Search Screen</Text>
-      <Text>We have found {results.length} results</Text>
+      <SearchBar
+        searchTerm={term}
+        onTermChange={setTerm}
+        onTermSubmit={searchApi}
+      />
+      {
+        errorMessage ?
+          <Text>{errorMessage}</Text>
+          : <Text>We have found {results.length} results</Text>
+      }
+
+
     </View>
   );
 };
