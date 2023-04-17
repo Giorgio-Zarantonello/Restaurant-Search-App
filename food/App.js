02a6a6b3d4090be646db364219e8ca8bceef5b47
Giorgio-Zarantonello <giorgio.zarantonello@gmail.com>
2023-04-17 18:43:52 +0200

    Navigation implemented on restaurant card

--- food/App.js
+++ food/App.js
@@ -1,15 +1,17 @@
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 import SearchScreen from "./src/screens/SearchScreen";
+import ResultShowScreen from "./src/screens/ResultShowScreen";
 
 const navigator = createStackNavigator({
-  Search : SearchScreen,
+  Search : SearchScreen,  
+  resultShow : ResultShowScreen
 },
 {
   initialRouteName : 'Search',
   // options for every different screen , global 
   defaultNavigationOptions: {
-    title: 'Just Zara'
+    title: 'Just Zara' , 
   }
 });
 
